0c0b590df1eea63a9bce44460ffdeb332d37010f
Daniel Fahlke <daniel@digitalmanufaktur.com>
2012-10-29 14:33:58 +0100

    more robust autoloading for bins

--- bin/composerCommandIntegrator.php
+++ bin/composerCommandIntegrator.php
@@ -1,8 +1,12 @@
 #!/usr/bin/env php
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
 
+if ((!@include __DIR__.'/../../../autoload.php') && (!@include __DIR__.'/../vendor/autoload.php')) {
+    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL);
+}
 
 $composer =\Composer\Factory::create(new  \Composer\IO\NullIO());
 
